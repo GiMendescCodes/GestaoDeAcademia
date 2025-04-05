f04c7bdc23be54e3a4e742e55cdc4eaaeb149542
GiMendescCodes <giovannamendessenai@gmail.com>
2025-04-05 15:46:41 -0300

    feat pag academia

--- src/models/Academia.java
+++ src/models/Academia.java
@@ -1,4 +1,23 @@
 package models;
 
-public class Academia {
-}
+import java.util.ArrayList;
+
+ class Academia {
+    private String nomeAcademia;
+    private ArrayList<Pessoa> pessoas;
+
+
+     public Academia(ArrayList<Pessoa> pessoas) {
+         this.pessoas = pessoas;
+         this.pessoas = new ArrayList<>();
+     }
+     public void adicionarPessoa(Pessoa pessoa){
+         pessoas.add(pessoa);
+     }
+    public void ListarPessoas(){
+        System.out.println("Lista de pssoas da academia: ");
+        for (Pessoa pessoasDaLista : pessoas){
+            System.out.println(pessoasDaLista.getDescricao());
+        }
+    }
+ }
