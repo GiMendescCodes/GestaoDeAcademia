a18df6bbe646762d5dcd3887ab8272ea6958f453
GiMendescCodes <giovannamendessenai@gmail.com>
2025-04-05 14:27:36 -0300

    Senai java POO2

---
+++ src/models/Pessoa.java
@@ -0,0 +1,23 @@
+package models;
+
+abstract class Pessoa {
+    protected String nomePessoa;
+    protected int idadePessoa;
+
+
+    public Pessoa(String nome, int idade) {
+        nomePessoa = nome;
+    idadePessoa = idade;
+    }
+
+    public String getNomePessoa() {
+        return nomePessoa;
+    }
+
+    public int getIdadePessoa() {
+        return idadePessoa;
+    }
+
+    public abstract String getDescricao();
+
+}
